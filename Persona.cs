041b4aed171b95d72749d22da1182781e3d85ad8
Cocalao <eileenmirandahenriquez@gmail.com>
2025-02-13 07:30:44 -0600

    Add files via upload

---
+++ Persona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploPersona3554568
+{
+    public class Persona
+    {
+        public Persona(string ojos, string altura, string peso, string sexo) 
+        {
+            this.Ojos=ojos;
+            this.Altura=altura;
+            this.Peso=peso;
+            this.Sexo=sexo;
+        }
+        public string Ojos;
+        public string Altura;
+        public string Peso;
+        public string Sexo;
+
+        //Acciones
+        public void Hablar(string texto)
+        {
+            //Se muestra la conversación a decir
+            Console.WriteLine("Voy a decir:" + texto);
+        }
+        public void Comer(double calorias)
+        {
+            //Se indican las calorias que gana al comer
+            Console.WriteLine("Ganando" + calorias + "calorias");
+            this.Peso += calorias / 1000;
+        }
+        public void Correr(double calorias)
+        {
+            //Se indica las calorias que se pierde al comer
+            Console.WriteLine("Perdiendo" + calorias + "calorias");
+            this.Peso = Convert.ToString(calorias / 1000);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploPersona3554568
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            //Instanciar a persona nadadora
+            PersonaNadadora persona1 = new PersonaNadadora("Verdes", "187", "85", "V");
+            Console.WriteLine("Descripción de la persona: ");
+            Console.WriteLine("Ojos: " + persona1.Ojos);
+            Console.WriteLine("Altura: "+persona1.Altura);
+            Console.WriteLine("Peso: "+persona1.Peso);
+            Console.WriteLine("Sexo: "+persona1.Sexo);
+            persona1.Hablar("Me llamo Borja");
+            Console.WriteLine("Color de mis ojos: "+persona1.Ojos);
+            persona1.Comer(1000);
+            Console.WriteLine("Mi peso actual es: " + persona1.Peso);
+            persona1.Nadar();
+            Console.WriteLine("Tras nadar peso: "+ persona1.Peso);
+
+
+            Console.ReadKey();
+        }
+    }
+}
